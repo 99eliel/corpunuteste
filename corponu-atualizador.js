db64e55de840d04e7bff03f695b241c218c6a2b6
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-26 18:53:16 -0300

    chore: alinhar atualizador à release 253

--- corponu-atualizador.js
+++ corponu-atualizador.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const LOCAL_RELEASE = "2026-08-25-manejo-calcinha-dedicado-252";
+  const LOCAL_RELEASE = "2026-08-26-manejo-calcinha-filtros-identidade-253";
   const INTERVALO_VERIFICACAO = 60 * 1000;
   const RELOAD_KEY = "corponu_web_release_recarregada";
   const MODULO_GRUPOS_FACCOES = ["corponu-faccoes-grupos-processos.js", "faccoes-grupos-processos", "Não foi possível carregar os grupos de processos das facções."];
